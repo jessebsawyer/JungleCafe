b54fd2e988e5ab988960a0ab4ddeaa848271c1ba
jessebsawyer <46061645+jessebsawyer@users.noreply.github.com>
2021-02-08 09:58:48 -0500

    changed paypal currency to CAD

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -56,7 +56,7 @@ const OrderScreen = ({ match, history }) => {
       const { data: clientId } = await axios.get('/api/config/paypal');
       const script = document.createElement('script');
       script.type = 'text/javascript';
-      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=CAD`;
       script.async = true;
       script.onload = () => {
         setSdkReady(true);
@@ -204,6 +204,7 @@ const OrderScreen = ({ match, history }) => {
                     <Loader />
                   ) : (
                     <PayPalButton
+                      currency='CAD'
                       amount={order.totalPrice}
                       onSuccess={successPaymentHandler}
                     />
