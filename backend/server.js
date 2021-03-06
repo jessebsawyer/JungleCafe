2a92255898d3c8f1ffee68bd3819ec9ceac72609
jessebsawyer <46061645+jessebsawyer@users.noreply.github.com>
2021-03-06 18:54:07 -0500

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 import colors from 'colors';
 import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
